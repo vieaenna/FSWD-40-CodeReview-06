c99eba24358d11ae5cb269b3671ef760902c41fc
Anna <anna0706@gmx.at>
2018-05-19 10:15:45 +0200

    basic

---
+++ js/script.js
@@ -0,0 +1,92 @@
+var collection =[
+	{
+		Title: 'Howl',
+		Type: 'Vinyl',
+		Author: 'Mighty Oaks',
+		Genre: 'Indie-Folk',
+		Publisher: 'Universal Music',
+		Image: "img/oaks.jpg",
+		Short Description: 'Three young men, from three countries, combining three part harmonies, resulting in one unique sound.'
+		Rating: '5'	
+	},
+
+	{
+		Title: 'New Estate',
+		Type: 'Vinyl',
+		Author: 'Giant Rooks',
+		Genre: 'Indie',
+		Publisher: 'Haldern Pop Recordings'
+		Image: "img/rooks.jpg",
+		Short Description: 'With a sound both instantly familiar, yet idiosyncratic enough so as to remain enigmatic, the band have hit upon a deft balance of the accessible and the utterly irresistible.'
+		Rating: '4' 	
+	},
+
+	{
+		Title: 'The Importance of Being Earnest',
+		Type: 'Book',
+		Author: 'Oscar Wilde',
+		Genre: 'Comedy',
+		Publisher: 'Other',
+		Image: "img/earnest.jpg",
+		Short Description: 
+		Rating: 5,	
+	},
+
+	{
+		Title: 'A Clockwork Orange',
+		Type: 'Book',
+		Author: 'Anthony Burgess',
+		Genre: 'Novel',
+		Publisher: 'William Heinemann',
+		Image: "img/Clockwork.jpg",
+		Short Description: 
+		Rating: 5,	
+	},
+
+	{
+		Title: 'Understanding Comics',
+		Type: 'Graphic Novel',
+		Author: 'Scott McCloud',
+		Genre: 'Non-Fiction',
+		Publisher: 'Amazing Heroes',
+		Image: "img/comics.jpg", 
+		Short Description: 
+		Rating: 4, 	
+	},
+
+	{
+		Title: 'Palestine',
+		Type: 'Graphic Novel',
+		Author: 'Joe Sacco',
+		Genre: 'Non-Fiction',
+		Publisher: 'Fantagraphics',
+		Image: "img/palestine.jpg",
+		Short Description: 
+		Rating: 4,	
+	},
+
+	{
+		Title: 'Friends',
+		Type: 'Series',
+		Author: 'David Crane & Marta Kauffman',
+		Genre: 'Sitcom',
+		Publisher: 'Warner Bros. Television',
+		Image: "img/friends.jpg",
+		Short Description: 
+		Rating: 5,	
+	},
+
+	{
+		Title: 'Silicon Valley',
+		Type: 'Series',
+		Author: 'Mike Judge',
+		Genre: 'Comedy',
+		Publisher: 'Warner Bros. Television',
+		Image: "img/silicon.jpg",
+		Short Description: 
+		Rating: 3, 	
+	},
+
+
+];
+
